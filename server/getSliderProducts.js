fa3bf34dc10bd59bf231173897f8dae549db2610
Nicewebworkbest <nicewebworkbest@gmail.com>
2020-09-21 08:19:29 -0700

    first commit

--- server/getSliderProducts.js
+++
@@ -1,7 +0,0 @@
-const { getSliderProductsByShop, addSliderProduct } = require('./src/service/slider_products.service');
-
-const getSliderProducts = async (ctx, accessToken, shop) => {
-    return await getSliderProductsByShop(shop);
-};
-  
-module.exports = getSliderProducts;
--- server/removeSliderProducts.js
+++
@@ -1,7 +0,0 @@
-const { removeProductsFromSlider } = require('./src/service/slider_products.service');
-
-const removeSliderProducts = async (productIds, shop) => {
-  return await removeProductsFromSlider(shop, productIds);
-};
-  
-module.exports = removeSliderProducts;
